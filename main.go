75121e2c48d16d9e58a3b0216a59c5713fefe147
Pathao Ltd <pathao@MacBook-Pro.local>
2022-11-10 05:53:01 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/hello", SayHello)
+
+	http.ListenAndServe("0.0.0.0:3001", handler)
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Hello World`)
+}
